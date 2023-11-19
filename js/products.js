d2f8d1a540b241b54c57188471dd8d84a63edb4a
Pvbaeza <129808763+Pvbaeza@users.noreply.github.com>
2023-11-19 03:08:29 -0300

    V.2 Creacion de carrito de compras 1.0

---
+++ js/products.js
@@ -0,0 +1,107 @@
+var servicios = [
+    {
+      id: 1,
+      titulo: "Limpieza facial profunda",
+      imagen: "../imagenes/limpiezaprofunda.jpeg",
+      precio: 12000,
+      descripcion: "Limpieza profunda, exfoliación y mascarillas acordes a tu tipo de piel."
+    },
+    {
+      id: 2,
+      titulo: "Maderoterapia",
+      imagen: "../imagenes/maderoterapia.jpeg",
+      precio: 13000,
+      descripcion: "Resalta tu figura con nuestra Maderoterapia! Utilizamos instrumentos de madera para esculpir tu figura."
+    },
+    {
+      id: 3,
+      titulo: "Dermapen",
+      imagen: "../imagenes/dermapen.jpeg",
+      precio: 14000,
+      descripcion: "Renueva tu piel con Dermapen! Microagujas estimulan colágeno para mejorar textura y reducir arrugas."
+    },
+    {
+      id: 4,
+      titulo: "Limpieza Facial Premium",
+      imagen: "../imagenes/limpiezapremium.jpeg",
+      precio: 15000,
+      descripcion: "Tratamiento de limpieza facial de alta gama para una experiencia exclusiva."
+    },
+    {
+      id: 5,
+      titulo: "Dermaplaning",
+      imagen: "../imagenes/dermaplaning.jpeg",
+      precio: 16000,
+      descripcion: "Técnica de exfoliación profunda que elimina el vello facial y las células muertas de la piel."
+    },
+    {
+      id: 6,
+      titulo: "Spa Capilar",
+      imagen: "../imagenes/spacapilar.jpeg",
+      precio: 17000,
+      descripcion: "Tratamiento para revitalizar y mejorar la salud del cuero cabelludo y el cabello."
+    },
+    {
+      id: 7,
+      titulo: "Pulido de Espalda",
+      imagen: "../imagenes/pulidoespalda.jpeg",
+      precio: 18000,
+      descripcion: "Exfoliación y tratamiento para una espalda suave y libre de impurezas."
+    },
+    {
+      id: 8,
+      titulo: "Masaje Relajante",
+      imagen: "../imagenes/masajerelajante.jpeg",
+      precio: 19000,
+      descripcion: "Disfruta de un masaje que alivia el estrés y promueve la relajación total."
+    },
+    {
+      id: 9,
+      titulo: "Tratamiento Reductor",
+      imagen: "../imagenes/tratamientoreductor.jpeg",
+      precio: 20000,
+      descripcion: "Reduce medidas y moldea tu figura con nuestro tratamiento reductor especializado."
+    },
+    {
+      id: 10,
+      titulo: "Tratamiento Reafirmante",
+      imagen: "../imagenes/tratamientoreafirmante.jpeg",
+      precio: 21000,
+      descripcion: "Reafirma y tonifica la piel para una apariencia más juvenil y firme."
+    },
+    {
+      id: 11,
+      titulo: "Tratamiento Anticelulitis",
+      imagen: "../imagenes/tratamientoanticelulitis.jpeg",
+      precio: 22000,
+      descripcion: "Combate la celulitis y mejora la textura de la piel con nuestro tratamiento especializado."
+    },
+    {
+      id: 12,
+      titulo: "Limpieza Facial para Pieles Acnéicas",
+      imagen: "../imagenes/limpiezaacneica.jpeg",
+      precio: 23000,
+      descripcion: "Tratamiento especializado para limpiar y purificar las pieles propensas al acné."
+    },
+    {
+      id: 13,
+      titulo: "Limpieza Facial Rejuvenecedora",
+      imagen: "../imagenes/limpiezarejuvenecedora.jpeg",
+      precio: 24000,
+      descripcion: "Renueva y revitaliza tu piel con nuestro tratamiento de limpieza facial rejuvenecedor."
+    },
+    {
+      id: 14,
+      titulo: "Limpieza Facial para Pieles Sensibles",
+      imagen: "../imagenes/pielsensible.jpeg",
+      precio: 25000,
+      descripcion: "Cuidado suave y específico para pieles sensibles, limpieza profunda sin irritación."
+    },
+    {
+      id: 15,
+      titulo: "Limpieza Facial Hidratante",
+      imagen: "../imagenes/limpiezahidratante.jpeg",
+      precio: 26000,
+      descripcion: "Proporciona a tu piel una hidratación profunda y revitalizante con nuestro tratamiento especializado."
+    }
+  ];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+const shopContent = document.getElementById("shopContent");
+
+const verCarrito = document.getElementById("ver-carrito");
+
+const modalcontainer = document.getElementById("modal-section")
+
+var carrito=[];  
+  
+servicios.forEach((servicios)=>{
+    let content = document.createElement("div");
+    content.className = "card todoslosservicios";
+    content.innerHTML=`
+        <img src="${servicios.imagen}">
+        <h3 class="tituloservicio">${servicios.titulo}</h3>
+        <p>${servicios.descripcion}</p>
+    `;
+    shopContent.append(content);
+
+//    <button id="botonreservar" class="botonreservar">Reservar</button>
+
+    let botonreservar = document.createElement("button");
+    botonreservar.innerHTML="Reservar";
+    botonreservar.className="botonreservar";
+
+    content.append(botonreservar);
+
+    botonreservar.addEventListener("click",() =>{
+
+        carrito.push({
+            id: servicios.id,
+            titulo: servicios.titulo,
+            imagen: servicios.imagen,
+            precio: servicios.precio,
+            descripcion: servicios.descripcion
+        });
+        console.log(carrito)
+    });
+});
+
+
 document.addEventListener("keyup", e => {
     if (e.target.matches("#buscador")) {
         const searchTerm = e.target.value.toLowerCase();
@@ -15,4 +55,50 @@ document.addEventListener("keyup", e => {
             }
         });
     }
+});
+
+verCarrito.addEventListener("click",()=>{
+    modalcontainer.innerHTML="";
+    modalcontainer.style.display="flex";
+
+    const modalHeader = document.createElement("div");
+    modalHeader.className="modal-header";
+    modalHeader.innerHTML=`
+    <h1 class="modal-header-titulo">Tus servicios</h1>  
+    `;
+    modalcontainer.append(modalHeader);
+
+    const modalbutton=document.createElement("h1");
+    modalbutton.innerHTML="🛒";
+    modalbutton.className="modal-header-button";
+
+    modalbutton.addEventListener("click",()=>{
+        modalcontainer.style.display="none";
+    })
+
+    modalHeader.append(modalbutton);
+
+
+    carrito.forEach((servicio)=>{
+        let contenidocarrito = document.createElement("div");
+        contenidocarrito.className="modal-content";
+        contenidocarrito.innerHTML=`
+        <img class="img-carrito" src="${servicio.imagen}">
+        <div class="servicios-carrito">
+        <h3>${servicio.titulo}</h3>
+        <p>${servicio.precio}</p>
+        </div>
+        `;
+
+        modalcontainer.append(contenidocarrito);
+
+    })
+
+    const total=carrito.reduce((acc,el)=> acc + el.precio,0);
+   
+    const totalcompra = document.createElement("div");
+    totalcompra.className="total-content";
+    totalcompra.innerHTML=`Total: $${total} 
+    <button class="botonpagar">Reservar</button>`;
+    modalcontainer.append(totalcompra);
 });
